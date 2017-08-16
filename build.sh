83b53baf208705a4854a306b714b35d73e1f1f12
growse <github@growse.com>
2017-08-16 19:16:25 +0100

    box version in build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 vagrant up --provider virtualbox
 vagrant package --output output.box
 vagrant destroy -f
-curl -o upload.txt -H "X-Atlas-Token: ${ATLAS_TOKEN}"-XPUT --upload-file output.box $(curl -H "X-Atlas-Token: ${ATLAS_TOKEN}" -XGET 'https://atlas.hashicorp.com/api/v1/box/growse/debian-jessie64-puppet4/version/1.0.0/provider/virtualbox/upload' | jq -r .upload_path)
+curl -o upload.txt -H "X-Atlas-Token: ${ATLAS_TOKEN}"-XPUT --upload-file output.box $(curl -H "X-Atlas-Token: ${ATLAS_TOKEN}" -XGET 'https://atlas.hashicorp.com/api/v1/box/growse/debian-jessie64-puppet4/version/$BOX_VERSION/provider/virtualbox/upload' | jq -r .upload_path)
 
